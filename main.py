4d17e194b47641cc8755d60ebd0f83a60a3e6052
Rahim Merchant <117778068+rahimmerchan@users.noreply.github.com>
2024-12-27 15:38:15 -0500

    Update main.py
    fix spacing line 27

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
      CORSMiddleware,
-     allow_origins=["*"],  # For development only. In production, specify your extension's origin
+     allow_origins=["*"],  # For development only. In production, specify your extension's origin
      allow_credentials=True,
      allow_methods=["*"],
      allow_headers=["*"],
